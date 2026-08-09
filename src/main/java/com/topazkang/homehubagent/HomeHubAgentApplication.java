5995efd8dbc282cfe98f1c63b7d7a25a3db210a6
TopazKang <lasksla@gmail.com>
2026-08-09 14:14:51 +0900

    init: 에이전트 프로젝트 초기 구성

---
+++ src/main/java/com/topazkang/homehubagent/HomeHubAgentApplication.java
@@ -0,0 +1,13 @@
+package com.topazkang.homehubagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeHubAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeHubAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/topazkang/homehubagent/HomeHubAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topazkang.homehubagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeHubAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
